add9bbbfb13b54af5cf182cd5d2f7e5835c08b38
Quillington <lauren.murach@gmail.com>
2021-01-05 12:29:20 -0500

    Bot now states a random fruit from a predeterminted list.

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import discord
 import configparser
+import random
 
 config = configparser.ConfigParser()
 config.read('config.ini')
-config['DEFAULT']['token']
 
 client = discord.Client()
 
+fruits = ['orange', 'apple', 'pear', 'banana', 'kiwi',]
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
@@ -17,7 +18,7 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    if message.content.startswith('$fruit'): 
+        await message.channel.send(random.choice(fruits))
 
 client.run(config['DEFAULT']['token'])
