34bf1376f5dc964cc3e97fffba27ccea512b8269
Quillington <lauren.murach@gmail.com>
2021-01-06 20:04:08 -0500

    Added dynamic counting of fruits rolled.

--- main.py
+++ main.py
@@ -14,6 +14,14 @@ fruits = {
     "kiwi" : "🥝"
 }
 
+userFruitDictionary = {
+
+}
+
+fruitCounters = {
+    
+}
+
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
@@ -26,8 +34,22 @@ async def on_message(message):
 
     if message.content.startswith('$fruit'): 
         fruitChoice = random.choice(list(fruits.keys()))
-        
+        if not (message.author.id in userFruitDictionary):
+            userFruitDictionary.update({message.author.id : []})
+        userFruitDictionary[message.author.id].append(fruitChoice)
         reply = await message.channel.send(fruitChoice)
         await reply.add_reaction(fruits[fruitChoice])
+
+
+    if message.content.startswith('$total'): 
+        for fruit in list(fruits.keys()):
+            fruitCounters.update({fruit : 0})
+        reply2 = ""
+        for item in userFruitDictionary[message.author.id]:
+            fruitCounters[item] += 1
+        for k in fruitCounters:
+            reply2 = reply2 + f", {fruitCounters[k]} {k}"
+        
+        await message.channel.send(f"During this session{reply2} have been rolled.")
 
 client.run(config['DEFAULT']['token'])
