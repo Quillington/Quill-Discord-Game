ce6faf7b12ae2bfc7e4bfde9706c4c3550a46419
Quillington <lauren.murach@gmail.com>
2021-01-21 18:34:22 -0500

    changed rolling functions

--- main.py
+++ main.py
@@ -15,25 +15,60 @@ fruits = {
     "kiwi" : "🥝"
 }
 
-userFruitDictionary = {
+users = {}
 
-}
+class UserInfo:
 
-fruitCounters = {
+    def __init__ (self):
+        self.fruits = []
+        self.currency = 20
+
+    def check_currency(self, num):
+        #pass to rolling functions before rolls
+        if self.currency >= num:
+            return True
+        else:
+            return False
+        
+    def remove_currency(self, num):
+        self.currency -= num
+
+    @staticmethod
+    def get_user (id):
+        if not (id in users):
+            users.update({id : UserInfo()})
+        return users[id] 
+
+#fruitCommands:
+def open_shop(user):
+    #opens up shop with previous rolls
+    if user.fruits == []:
+        roll_fruits(user)
+    else:
+        return user.fruits
+
+def roll_fruits(user):
+    #add random fruits to a list and returns
+    fruitRolls = ""
+    totalRolls = [] 
+    if user.check_currency(10):
+        user.remove_currency(10)
+        user.fruits = []
+        i = 4
+        while i > 0:
+            fruitRolls = random.choice(list(fruits.values()))
+            user.fruits.append(fruitRolls)
+            totalRolls.append(fruitRolls)
+            i -= 1
+        return totalRolls
+    else:
+        return ["You do not have enough money to roll fruit."]
     
-}
+        
+        
 
-class userInfo:
 
-    def __init__ (self, user):
-        self.user = user
-    
-    def get_user (self, user):
-        if not (user in userFruitDictionary):
-            userFruitDictionary.update(
-                {user : [] }
-                )
-                
+
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
@@ -41,23 +76,23 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.author == client.user:
-        return
+    user = UserInfo.get_user(message.author.id)
 
     if message.content.startswith('$fruit'): 
-        fruitChoice = random.choice(list(fruits.keys()))
-        u = userInfo(message.author.id)
-        u.get_user(message.author.id)
-        userFruitDictionary[message.author.id].append(fruitChoice)
-        reply = await message.channel.send(fruitChoice)
-        await reply.add_reaction(fruits[fruitChoice])
+        
+        reply = ""
+        for i in roll_fruits(user):
+            reply = reply + i
+        await message.channel.send(reply)
 
 
     if message.content.startswith('$total'): 
+        fruitCounters = {}
+
         for fruit in list(fruits.keys()):
             fruitCounters.update({fruit : 0})
         reply2 = ""
-        for item in userFruitDictionary[message.author.id]:
+        for item in user.fruits:
             fruitCounters[item] += 1
         for k in fruitCounters:
             reply2 = reply2 + f", {fruitCounters[k]} {k}"
