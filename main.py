6fccb1f9ac89f73743746f1cf8606383e510da12
Quillington <lauren.murach@gmail.com>
2021-01-23 11:39:22 -0500

    Added comments and fixed bug involving empty shop

--- main.py
+++ main.py
@@ -24,6 +24,8 @@ class UserInfo:
 
     def __init__ (self):
         self.fruits = []
+        self.pickedFruits = []
+        self.fruitLimit = 5
         self.currency = 50
         self.lastMessage = 0
         self.firstMessage = True
@@ -48,7 +50,6 @@ class UserInfo:
         return users[id] 
 
 #fruitCommands:
-
 def roll_fruits(user, noRoll):
     #add random fruits to a list and returns
     if (not user.fruits == []) and (noRoll):
@@ -70,6 +71,16 @@ def roll_fruits(user, noRoll):
         else:
             return False
 
+def pick_fruits(user, reaction):
+    #Picks fruit if there is room in storage
+    if len(user.pickedFruits) >= user.fruitLimit:
+        return False
+    for f in user.fruits:
+            if reaction.emoji == f:
+                user.pickedFruits.append(f)
+                user.fruits.remove(f)
+                return True
+
 #Message Commands:
 async def send_roll(user, message, checkMessage):
     fruitList = "".join(user.fruits)
@@ -84,9 +95,10 @@ async def send_roll(user, message, checkMessage):
     for f in user.fruits:
         await message.add_reaction(f)
     await message.add_reaction("🔁")
+
     
                 
-
+#Events:
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
@@ -96,7 +108,7 @@ async def on_ready():
 @client.event
 async def on_message(message):
     user = UserInfo.get_user(message.author.id)
-
+    #Pulls up shop
     if message.content.startswith('$fruit'):
         if roll_fruits(user, True):
             await send_roll(user, message, True)
@@ -106,14 +118,30 @@ async def on_message(message):
 @client.event
 async def on_reaction_add(reaction, author):
     user = UserInfo.get_user(author.id)
+    #Edits shop message with new rolls when repeat is reacted
     if user.lastMessage == reaction.message.id and reaction.emoji == "🔁":
         if roll_fruits(user, False):
             await send_roll(user, reaction.message, False)
         else:
             await reaction.message.channel.send("You can't afford to roll fruit.")
 
+    #Handles picking and rolling when there is only one fruit left.
+    elif (user.lastMessage == reaction.message.id) and (reaction.emoji in user.fruits) and (len(user.fruits) == 1):
+        if pick_fruits(user, reaction) and roll_fruits(user, False):
+            await send_roll(user, reaction.message, False)
+        elif not pick_fruits(user, reaction):
+            await reaction.message.channel.send(f"You have too many fruits in storage ({user.fruitLimit}).")
+        else:
+            await reaction.message.channel.send("You can't afford to roll fruit.")
 
+    #Normal picking and shop refresh
+    elif (user.lastMessage == reaction.message.id) and (reaction.emoji in user.fruits):
+        if pick_fruits(user, reaction):
+            await send_roll(user, reaction.message, False)
+        else:
+            await reaction.message.channel.send(f"You have too many fruits in storage ({user.fruitLimit}).")
 
+        
 
 
 client.run(config['DEFAULT']['token'])
